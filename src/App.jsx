907d6ec139756d20f16e36d11121ce1601bf56be
Viktoriya-Frank <playviktory@gmail.com>
2025-03-27 18:56:01 +0100

    change name

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,30 @@ import {TwitterContext} from "./utils/context.jsx";
 function App() {
     const [user, setUser] = useState({
         name: 'Monster',
-        avatar: 'https://gravatar.com/avatar/000?d=retro'
+        avatar: 'https://gravatar.com/avatar/000?d=monsterid'
     });
 
     const [stats, setStats] = useState({
-        followers: 10,
-        following: 20
-    })
+        followers: 0,
+        following: 0
+    });
+
+    const changeAvatar = url => {
+        setUser(prevState => ({...prevState, avatar: url || prevState.avatar}));
+    }
+
+    const changeName = name => {
+        setUser(prevState => ({...prevState, name: name || prevState.name}));
+    }
 
   return (
    <div className={`app`}>
-       <TwitterContext.Provider value={{
-           user, stats
+       <TwitterContext value={{
+           user, stats, changeAvatar, changeName
        }}>
        <Navigation />
        <Body/>
-       </TwitterContext.Provider>
+       </TwitterContext>
    </div>
   )
 }
--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -2,19 +2,21 @@ import React, {useContext} from 'react';
 import {TwitterContext} from "../utils/context.jsx";
 
 const Avatar = ({size}) => {
-    const {user, setUser} = useContext(TwitterContext);
-
-
-    const click = () => {
-        const newAvatar = prompt('Please, enter URL of new avatar:', user.avatar);
-        if(newAvatar && newAvatar !== user.avatar) {
-            setUser(prevUser => ({...prevUser, avatar: newAvatar}));
-        }
-    }
-
-
+    const {user, changeAvatar, changeName} = useContext(TwitterContext);
     return (
-        <img className={`user-avatar ${size ?? ''}`} src={user.avatar} alt={user.name} onClick={click} />
+        <img
+            onClick={() => {
+                const url = prompt("Enter Avatar URL");
+                changeAvatar(url);
+            }}
+            onContextMenu={e => {
+                e.preventDefault();
+                const name = prompt("Enter User Name");
+                changeName(name);
+            }}
+            className={`user-avatar ${size || ''}`}
+            src={user.avatar} alt={user.name}
+        />
     );
 };
 
